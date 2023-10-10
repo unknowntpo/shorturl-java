8c2afe9de4511947f454467ba1aa6d57a26cf76f
unknowntpo <e850506@gmail.com>
2023-10-10 15:40:21 +0800

    add starter config

---
+++ src/main/java/com/example/shorturljava/ShorturlJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.shorturljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorturlJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShorturlJavaApplication.class, args);
+    }
+
+}
